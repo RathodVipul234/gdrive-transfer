38b47133065327e6af937ec926fbe6e272ae4f5c
RathodVipul234 <rathodv9229@gmail.com>
2025-06-16 22:45:59 +0530

    fix

--- gdrive_transfer/urls.py
+++ gdrive_transfer/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
+@csrf_exempt
+@require_http_methods(["GET", "HEAD"])
 def health_check(request):
     return HttpResponse("OK", status=200)
 
